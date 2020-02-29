8db62db22132c1ebb69278acb00101f517f3e920
Dawidek <apple@Apples-iMac.connect>
2020-02-29 22:45:33 +0000

    feat: added button and inputfield with basic styling

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Add Task' style={styles.textInput}/>
+        <Button title="Add"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
@@ -16,4 +22,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderWidth: 1,
+    padding: 10,
+    margin: 5
+  },
+  inputContainer: {
+    flexDirection: 'row'
+  }
 });
