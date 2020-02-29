66c07a77a1304828fffae3792346e3ddacdea81a
Dawidek <apple@Apples-iMac.connect>
2020-02-29 23:05:33 +0000

    feat: now there is a list of tasks

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredTask, setEnteredTask] = useState('')
+  const [taskList, setTaskList] = useState([])
+
+
+  function taskInputHandler(enteredText) {
+    setEnteredTask(enteredText)
+  }
+
+  function addTaskHandler() {
+    setTaskList([...taskList, enteredTask])
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Add Task' style={styles.textInput}/>
-        <Button title="Add"/>
+        <TextInput
+          placeholder='Set Task'
+          style={styles.textInput}
+          onChangeText={taskInputHandler}
+          value={enteredTask}/>
+        <Button title="ADD" onPress={addTaskHandler}/>
       </View>
       <View>
-
+        {taskList.map((task) => <Text>{task}</Text> )}
       </View>
     </View>
   );
@@ -28,6 +44,7 @@ const styles = StyleSheet.create({
     margin: 5
   },
   inputContainer: {
+    alignItems: 'center',
     flexDirection: 'row'
   }
 });
