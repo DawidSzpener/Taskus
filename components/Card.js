a4e05e234c2820ee05a2079732dff3a53bb1befe
Dawidek <apple@Apples-iMac.local>
2020-03-03 12:00:15 +0000

    style: added some basic card style, shadows etc.

--- components/Card.js
+++ components/Card.js
@@ -9,6 +9,19 @@ const Card = props => {
 
 const styles = StyleSheet.create({
   card: {
+    width: 300,
+    maxWidth: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: { width:0 , height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: 'white',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10
   }
 })
 
