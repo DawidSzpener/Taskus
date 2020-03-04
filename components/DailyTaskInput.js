277c038cfb8b698f782a728db887e44f2a4a003f
Dawidek <dawidszpener141@gmail.com>
2020-03-04 10:01:22 +0000

    style: added style for daily task input modal

--- components/DailyTaskInput.js
+++ components/DailyTaskInput.js
@@ -40,4 +40,32 @@ const DailyTaskInput = props => {
   )
 }
 
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput: {
+    height: 40,
+    width: '80%',
+    flexDirection: 'row',
+    borderWidth: 1,
+    padding: 5,
+    margin: 5
+  },
+  buttonContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 5,
+    margin: 5
+  },
+  button: {
+    width: '80%',
+    flex: 1,
+    margin: 5
+  }
+})
+
 export default DailyTaskInput
