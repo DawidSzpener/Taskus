f7146a180cce946f30ac93bd8033e499dd5135af
Dawidek <dawidszpener141@gmail.com>
2020-03-10 15:01:16 +0000

    feat: defining routes for backend server

---
+++ backend/routes/dailys.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+let Dailys = require('../models/dailys.model')
+
+router.route('/').get((req, res) => {
+  Dailys.find()
+    .then(dailys => res.json(dailys))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/add').post((req, res) => {
+  const text = req.body.text
+  
+  const newDaily = new Dailys({text})
+
+  newDaily.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error:' + err))
+})
+
+module.exports = router
---
+++ backend/routes/tasks.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+let Tasks = require('../models/tasks.model')
+
+router.route('/').get((req, res) => {
+  Tasks.find()
+    .then(tasks => res.json(tasks))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/add').post((req, res) => {
+  const text = req.body.text
+  
+  const newTask = new Tasks({text})
+
+  newTask.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error:' + err))
+})
+
+module.exports = router
